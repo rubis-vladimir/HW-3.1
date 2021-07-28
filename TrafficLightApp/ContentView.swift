bbc2f2beb49fad2f8362ad41173a69f128a04304
rubis-vladimir <rubis-vladimir>
2021-07-28 09:43:45 +0300

    Initial Commit

---
+++ TrafficLightApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightApp
+//
+//  Created by Владимир Рубис on 28.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLightApp/TrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightAppApp.swift
+//  TrafficLightApp
+//
+//  Created by Владимир Рубис on 28.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
